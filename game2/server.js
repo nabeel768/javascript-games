a01a095b976cd425149d3d1ff8524a52570e402a
Nabeel Khan <nabeel.kay@gmail.com>
2023-01-01 19:54:29 +0000

    refactored and pretified

--- game2/server.js
+++ game2/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 const app = express();
 app.use(express.static(__dirname));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(3000);
